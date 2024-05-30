90a9c0f9fe4354c4fcad3ebe9b34876462529136
Plamen Ivanov <65338982+k1lgor@users.noreply.github.com>
2024-05-30 21:56:21 +0300

    Feat/add nox testing (#6)
    * refactor(ci.yaml): replace tox with nox in GitHub Actions workflow
    
    * feat: add noxfile.py for test automation with Nox
    
    * feat(.gitignore): add .nox/ to ignored directories
    
    * feat(pyproject.toml): add nox dependency

---
+++ noxfile.py
@@ -0,0 +1,18 @@
+import nox
+
+nox.options.env_fallback_enabled = True
+nox.options.force_venv_creation = True
+
+
+@nox.session
+def tests(session):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.run(
+        "pytest",
+        "--cov=gpt_engineer",
+        "--cov-report=xml",
+        "-k",
+        "not installed_main_execution",
+        env={"OPENAI_API_KEY": session.env.get("OPENAI_API_KEY")},
+    )
